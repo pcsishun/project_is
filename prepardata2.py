ad158de8ad2be642869ad9ce23df1b217058709a
pcsishun <pcsishun@Gmail.com>
2021-07-26 20:10:04 +0700

    update

--- prepardata2.py
+++ prepardata2.py
@@ -5,24 +5,6 @@ import os
 
 pytesseract.tesseract_cmd = r'C:\\Program Files\Tesseract-OCR\tesseract.exe' ## change path 
 
- ## Scan == 8 
- ## QR == 10 
- ## TF == 7 
-
-## โหลดรูปมาจัดทำให้อย่ในรูปแบบของ black and white 
-img_convert = cv2.imread('./image_krungsri/test3.jpg') # img paths  
-
-# convert into gray scale.
-gray_scale = cv2.cvtColor(img_convert,cv2.COLOR_RGB2GRAY)
-
-# convert gray scale to black and with by using threshold.
-thresh, img_black_white  = cv2.threshold(gray_scale, 120, 240, cv2.THRESH_BINARY)
-
-# Save image 
-cv2.imwrite('useImage.jpg', img_black_white)
-img = cv2.imread('./useImage.jpg')
-image_data = pytesseract.image_to_data(img, output_type=Output.DICT, lang="eng+tha")
-
 
 def findWord(data):
     for i in data:
@@ -110,7 +92,89 @@ def concatStringData(data_img, type_slip):
     return CC; 
 
 
+def tagLabelToData(eslipType, data_overall_image):
 
+    data_with_label = []
+    passing = 0
+    eslipType = eslipType[0]
+
+    for i,data in enumerate(data_overall_image):
+        
+        level = data[0]
+        page_num = data[1]
+        block_num = data[2]
+        par_num = data[3]
+        line_num = data[4]
+        word_num = data[5]
+        x = data[6]
+        y = data[7]
+        w = data[8]
+        h = data[9]
+        not_use_logo = data[10]
+        word = data[11]
+        
+        if data[10] == 1:
+            label = 0 
+            data_with_label.append([eslipType, level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
+        
+        elif data[10] == 0:
+            label_categorical = [["1",1], ["2",2], ["3",3], ["4",4], ["5",5], ["6",6], ["7",7], ["8",8], ["9",9], ["10",10], ["11",11], ["12",12], ["13",13], ["14",14], ["15",15], ["16",16], ["17",17], ["18",18], ["19",19], ["20",20]]
+            alert = []
+            
+            if data[0] == 4:
+                label = 0
+                print(passing)
+                print("data[0] == 4: ",[eslipType, level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
+                data_with_label.append([eslipType, level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
+                    
+            elif data[0] == 5:
+                label = label_categorical[passing][1]
+                print(passing)
+                print("data[0] == 5: ",[eslipType, level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
+                data_with_label.append([eslipType, level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
+                alert.append(1)
+                    
+            try:
+                print(alert[0])
+                try:
+                    data_overall_image[i + 1][0]
+                    print(data[0], data_overall_image[i+1][0])
+                    if data[0] == 5 and data_overall_image[i + 1][0] == 4:
+                        print("Try: ",data[0], data_overall_image[i + 1][0])
+                        passing = passing + 1
+                        print("passing",passing)
+                except :
+                    # print("Over array")
+                    pass
+            except :
+                # print("No alert")
+                pass
+
+    return data_with_label
+
+#################################################################################
+#################################################################################
+#################################################################################
+#################################################################################
+
+
+ ## Scan == 8 
+ ## QR == 10 
+ ## TF == 7 
+
+## โหลดรูปมาจัดทำให้อย่ในรูปแบบของ black and white 
+img_convert = cv2.imread('./image_krungsri/test3.jpg') # img paths  
+
+# convert into gray scale.
+gray_scale = cv2.cvtColor(img_convert,cv2.COLOR_RGB2GRAY)
+
+# convert gray scale to black and with by using threshold.
+thresh, img_black_white  = cv2.threshold(gray_scale, 120, 240, cv2.THRESH_BINARY)
+
+# Save image 
+cv2.imwrite('useImage.jpg', img_black_white)
+img = cv2.imread('./useImage.jpg')
+image_data = pytesseract.image_to_data(img, output_type=Output.DICT, lang="eng+tha")
 
 
  
@@ -212,79 +276,11 @@ for i in AA:
 ##################
 eslipType = eslipType_krungsri(image_data)
 CC = concatStringData(BB, eslipType[0])
+data_with_label = tagLabelToData(eslipType, data_overall_image)
+ 
 
-for i in data_overall_image:
-    print(i)
-
-print('\n')
-for i in BB:
-    print(i)
-
-print('\n')
-data_with_label = []
-passing = 0
-
-def tagLabelToData(eslipType, data_overall_image):
-
-    data_with_label = []
-    passing = 0
-
-    for i,data in enumerate(data_overall_image):
-        
-        level = data[0]
-        page_num = data[1]
-        block_num = data[2]
-        par_num = data[3]
-        line_num = data[4]
-        word_num = data[5]
-        x = data[6]
-        y = data[7]
-        w = data[8]
-        h = data[9]
-        not_use_logo = data[10]
-        word = data[11]
-    
-        if eslipType[0] == "QR":
-            if data[10] == 1:
-                ## 0 = ไม่ใช้งาน ## 
-                label = 0 
-                # print("eslipType == QR: ",[level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
-                data_with_label.append([level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
-
-            elif data[10] == 0:
-                label_categorical = [["Timing",1], ["UserTF",2], ["AccountTF",3], ["Amount",4], ["RefCode",5], ["RefCode",6], ["RefCode",7], ["RefCode",8], ["RefCode",9], ["RefCode",10]
-                , ["RefCode",11], ["RefCode",12], ["RefCode",13], ["RefCode",14], ["RefCode",15]]
-                alert = []
-
-                if data[0] == 4:
-                    label = 0
-                    print(passing)
-                    print("data[0] == 4: ",[level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
-                    data_with_label.append([level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
-                
-                elif data[0] == 5:
-                    label = label_categorical[passing][1]
-                    print(passing)
-                    print("data[0] == 5: ",[level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
-                    data_with_label.append([level, page_num, block_num, par_num, line_num, word_num, x, y ,w, h, not_use_logo, word,  label])
-                    alert.append(1)
-                
-                try:
-                    print(alert[0])
-                    try:
-                        data_overall_image[i + 1][0]
-                        print(data[0], data_overall_image[i+1][0])
-                        if data[0] == 5 and data_overall_image[i + 1][0] == 4:
-                            print("Try: ",data[0], data_overall_image[i + 1][0])
-                            passing = passing + 1
-                            print("passing",passing)
-                    except :
-                        # print("Over array")
-                        pass
-                except :
-                    # print("No alert")
-                    pass
-    return data_with_label
+for i, word in enumerate(CC):
+    print("index: ",i ,": ",word)
 
 print('\n')
 for i in data_with_label:
